30dbbaabfbda980810f08583a9f2435805c068b9
Alkis Gotovos <alkis.gotovos@gmail.com>
2025-06-03 13:39:23 +0200

    Add button and input styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 export default function Home() {
   return (
-    <div className="flex min-h-dvh flex-col items-center justify-center gap-y-4">
-      <Button className="txt-preset-5">Sign Up</Button>
-      <Button className="txt-preset-5" disabled>
-        Sign Up
-      </Button>
+    <div className="flex min-h-dvh flex-col items-center justify-center gap-y-8">
+      <Button>Sign Up</Button>
+      <Button disabled>Sign Up</Button>
+      <Button variant="secondary">Upload</Button>
+      <Input placeholder="name@mail.com" className="w-64" />
     </div>
   )
 }
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,15 +10,13 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-blue-600 text-neutral-0 hover:bg-blue-700 disabled:bg-blue-200 outline-none focus-visible:ring-blue-700 focus-visible:ring-2 focus-visible:ring-offset-[3px]',
-      },
-      size: {
-        default: 'px-400 py-150 rounded-10',
+          'txt-preset-5 bg-blue-600 text-neutral-0 hover:bg-blue-700 disabled:bg-blue-200 outline-none focus-visible:ring-blue-700 focus-visible:ring-2 focus-visible:ring-offset-[3px] px-400 py-150 rounded-10',
+        secondary:
+          'txt-preset-6 bg-neutral-0 text-neutral-900 border border-neutral-300 hover:border-neutral-900 outline-none focus-visible:ring-blue-700 focus-visible:ring-2 focus-visible:ring-offset-[3px] px-200 py-100 rounded-8',
       },
     },
     defaultVariants: {
       variant: 'default',
-      size: 'default',
     },
   }
 )
@@ -26,7 +24,6 @@ const buttonVariants = cva(
 function Button({
   className,
   variant,
-  size,
   asChild = false,
   ...props
 }: React.ComponentProps<'button'> &
@@ -38,7 +35,7 @@ function Button({
   return (
     <Comp
       data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(buttonVariants({ variant, className }))}
       {...props}
     />
   )

e4e315c10a5e820255fd1e23bd541a820c1afb2a
Alkis Gotovos <alkis.gotovos@gmail.com>
2025-07-17 14:49:53 +0200

    Update today mood layout

--- components/TodayMoodDisplay.tsx
+++ components/TodayMoodDisplay.tsx
@@ -1,16 +1,18 @@
 import { DataPoint, Mood, Sleep } from '@/types'
 import DisplayCard from '@/components/DisplayCard'
-import { getMoodIcon, getMoodText } from '@/lib/utils'
-import Spacer from '@/components/Spacer'
+import { cn, getMoodIcon, getMoodText } from '@/lib/utils'
 import IconQuote from '@/assets/images/icon-quote.svg'
 import IconSleep from '@/assets/images/icon-sleep.svg'
 import IconReflection from '@/assets/images/icon-reflection.svg'
 
 export default function TodayMoodDisplay({ data }: { data: DataPoint }) {
   return (
-    <div className="flex w-full flex-col gap-y-250">
-      <MoodCard mood={data.mood} />
-      <div className="flex flex-col gap-y-250">
+    <div className="flex w-full flex-col gap-250 desktop:flex-row desktop:gap-400">
+      <MoodCard
+        className="tablet:h-[340px] desktop:w-[670px]"
+        mood={data.mood}
+      />
+      <div className="flex h-[340px] flex-col gap-y-250">
         <SleepCard sleep={data.sleep} />
         <ReflectionCard comment={data.comment} tags={data.tags} />
       </div>
@@ -18,26 +20,32 @@ export default function TodayMoodDisplay({ data }: { data: DataPoint }) {
   )
 }
 
-function MoodCard({ mood }: { mood: Mood }) {
+function MoodCard({ mood, className }: { mood: Mood; className?: string }) {
   const Icon = getMoodIcon(mood, 'color')
   const moodText = `“${getMoodText(mood)}”`
 
   return (
-    <DisplayCard className="items-center gap-y-0 px-200 py-400">
-      <span className="text-center txt-preset-3 text-[32px] text-neutral-900/70">
-        {'I’m feeling'}
-      </span>
-      <span className="text-center txt-preset-2 text-[40px] capitalize">
-        {mood}
-      </span>
-      <Spacer className="h-400" />
-      <Icon className="size-[200px]" />
-      <Spacer className="h-400" />
-      <IconQuote className="size-6" />
-      <Spacer className="h-200" />
-      <span className="text-center txt-preset-6-italic text-pretty">
-        {moodText}
-      </span>
+    <DisplayCard
+      className={cn(
+        'relative items-center gap-y-400 overflow-hidden px-200 py-400 tablet:items-start tablet:justify-between tablet:px-400',
+        className
+      )}
+    >
+      <div className="flex flex-col items-center tablet:items-start">
+        <span className="text-center txt-preset-3 text-[32px] text-neutral-900/70">
+          {'I’m feeling'}
+        </span>
+        <span className="text-center txt-preset-2 text-[40px] capitalize">
+          {mood}
+        </span>
+      </div>
+      <Icon className="size-[200px] tablet:absolute tablet:top-[50px] tablet:right-10 tablet:size-[320px]" />
+      <div className="flex flex-col items-center gap-200 tablet:w-[246px] tablet:items-start tablet:gap-150">
+        <IconQuote className="size-6" />
+        <span className="text-center txt-preset-6-italic text-pretty tablet:text-start">
+          {moodText}
+        </span>
+      </div>
     </DisplayCard>
   )
 }
@@ -62,14 +70,14 @@ function ReflectionCard({
   tags?: string[]
 }) {
   return (
-    <DisplayCard className="gap-y-200 px-250 py-250">
+    <DisplayCard className="flex-grow gap-y-200 px-250 py-250">
       <div className="flex items-center gap-x-150">
         <IconReflection className="size-[22px] shrink-0 fill-neutral-600" />
         <span className="txt-preset-6 text-neutral-600">
           Reflection of the day
         </span>
       </div>
-      <span className="min-h-20 txt-preset-6">{comment}</span>
+      <div className="flex-grow txt-preset-6">{comment}</div>
       {tags && tags.length > 0 && (
         <div className="flex flex-wrap gap-x-150">
           {tags.map((tag) => (
